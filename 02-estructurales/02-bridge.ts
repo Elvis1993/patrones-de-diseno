168d0161e5a59b7fb362a3527eb31351df0bb974
Elvis Vargas <elvisv9307@gmail.com>
2026-01-29 08:46:53 -0500

    comentarios de la clase

--- 02-estructurales/02-bridge.ts
+++ 02-estructurales/02-bridge.ts
@@ -10,75 +10,83 @@
  * https://refactoring.guru/es/design-patterns/bridge
  */
 
-import { COLORS } from '../helpers/colors.ts';
-
+import { COLORS } from "../helpers/colors.ts";
+//“Cualquier que implemente habilidad DEBE tener un método use()”.
 interface Ability {
-    use(): void;
+  use(): void;
 }
 
+//clases que tienen implementado el metodo use()
 class SwordAttack implements Ability {
-    use():void{
-        console.log('ATaca con una espada ferozmente');
-    }
+  use(): void {
+    console.log("ATaca con una espada ferozmente");
+  }
 }
+
+//clases que tienen implementado el metodo use()
 
 class AxeAttack implements Ability {
-    use():void{
-        console.log('ATaca con una hacha brutalmente');
-    }
+  use(): void {
+    console.log("ATaca con una hacha brutalmente");
+  }
 }
 
+//clases que tienen implementado el metodo use()
 
-class MagicSpell implements Ability{
-    use():void{
-        console.log('Lanza un hechizo magico poderoso');
-    }
+class MagicSpell implements Ability {
+  use(): void {
+    console.log("Lanza un hechizo magico poderoso");
+  }
 }
 
-class FireBallSpell implements Ability{
-    use():void{
-        console.log('Lanza una bola de fuego');
-    }
+//clases que tienen implementado el metodo use()
+
+class FireBallSpell implements Ability {
+  use(): void {
+    console.log("Lanza una bola de fuego");
+  }
 }
 
-abstract class Character{
-    protected ability: Ability;
+  //Esta es la clase de puente (character ni sabe como se ataca solo sabe que tiene algo que cumple Ability) plantilla
+abstract class Character {
+  protected ability: Ability;
 
-    constructor(ability: Ability){
-        this.ability = ability;
-    }
+  constructor(ability: Ability) {
+    this.ability = ability;
+  }
 
-    setAbility(ability: Ability):void{
-        this.ability = ability;
-    }
+  setAbility(ability: Ability): void {
+    this.ability = ability;
+  }
 
-    abstract performAbility():void;
+  abstract performAbility(): void;
 }
 
-class Warrior extends Character{
-    override performAbility(): void{
-        console.log('El guerrero esta listo para luchar');
-        this.ability.use();
-    }
+//clases concretas (no saben si es SwordAttack, AxeAttack solo dicen this.ability.use())
+class Warrior extends Character {
+  override performAbility(): void {
+    console.log("El guerrero esta listo para luchar");
+    this.ability.use();
+  }
 }
 
-class Mage extends Character{
-    override performAbility(): void {
-        console.log("EL mago prepara su magia");
-        this.ability.use();
-    }
+//clases concretas (no saben si es MagicSpell, FireBallSpell solo dicen this.ability.use())
+class Mage extends Character {
+  override performAbility(): void {
+    console.log("EL mago prepara su magia");
+    this.ability.use();
+  }
 }
 
-function main(){
-    const warrior = new Warrior(new SwordAttack());
-    warrior.performAbility();
+function main() {
+  const warrior = new Warrior(new SwordAttack());
+  warrior.performAbility();
 
-    warrior.setAbility(new AxeAttack());
-    warrior.performAbility();
+  warrior.setAbility(new AxeAttack());
+  warrior.performAbility();
 
-    const mage = new Mage( new MagicSpell());
-    mage.performAbility();
-
+  const mage = new Mage(new MagicSpell());
+  mage.performAbility();
 }
 
 main();
